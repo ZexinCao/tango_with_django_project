419afe4874a8ba378af397c7cfac95f5cca3a73c
Zexin Cao <2293577c@student.gla.ac.uk>
2019-02-09 14:59:58 +0000

    Chapter 6.2

--- rango/views.py
+++ rango/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from rango.models import Category
+
 def index(request):
-    context_dict = {'boldmessage': "1, 2, 3, 4!"}
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
     return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    return HttpResponse("Rango says here is the about page!")
