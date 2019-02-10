a9384bc354dbf796d63c891921487d502b7aa014
Zexin Cao <2293577c@student.gla.ac.uk>
2019-02-10 19:12:05 +0000

    Chapter 8

--- rango/views.py
+++ rango/views.py
@@ -10,14 +10,13 @@ from rango.forms import CategoryForm
 
 from rango.forms import PageForm
 
+from django.core.urlresolvers import reverse
+
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
     context_dict = {'categories': category_list, 'pages': page_list}
     return render(request, 'rango/index.html', context=context_dict)
-
-def about(request):
-    return HttpResponse("Rango says here is the about page!")
 
 def show_category(request, category_name_slug):
     context_dict = {}
@@ -32,6 +31,11 @@ def show_category(request, category_name_slug):
         context_dict['pages'] = None
     
     return render(request, 'rango/category.html', context_dict)
+
+def about(request):
+    print(request.method)
+    print(request.user)
+    return render(request, '/rango/about.html', {})
 
 def add_category(request):
     form = CategoryForm()
